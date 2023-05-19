b054409e4213df18f77008b9e620aafc3c44e907
Luciano <lucianoalbertop@hotmail.com>
2023-05-19 01:29:11 -0300

    Config Project + Base CRUD + HomePage WEB/MOBILE

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree } from 'next/font/google'
+import { ReactNode } from 'react'
+
+const baiJamjuree = BaiJamjuree({ subsets: ['latin'], weight:'700', variable:'--font-bai-jamjuree' })
+const roboto = Roboto({ subsets: ['latin'], variable:'--font-roboto' })
+
+
+export const metadata = {
+  title: 'NLW  SpaceTime',
+  description: 'Project CapsuleTime - NLW  SpaceTime (React, Next.js, TailwindCSS, Typescript)',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans bg-gray-900 text-gray-100`}>{children}</body>
+    </html>
+  )
+}
